59a503073d05823efed0f21507e8c46c20079112
luyeewong <wongluyee1015@hotmail.com>
2023-01-07 21:09:39 +0900

    Add operate function

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-function add (a,b) {
-	return a+b;
+function add (a, b) {
+	return a + b;
 };
 
-function subtract (a,b) {
-	return a-b;
+function subtract (a, b) {
+	return a - b;
 };
 
-function multiply(a,b) {
-	return a*b;
+function multiply (a, b) {
+	return a * b;
 };
 
-function divide(a,b){
-    return a/b;
+function divide (a, b) {
+    return a / b;
+}
+
+function operate (operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+
+    if (operator === '+'){
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === '*') {
+        return multiply(a, b);
+    } else if (operator === '/') {
+        return (b === 0)? null : divide(a, b);
+    } else {
+        return "Error";
+    }
 }
