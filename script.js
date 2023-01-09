23900ccb9cc24b1961b36fe79ea9c0de9bfd6a3f
Luyee Wong <wongluyee1015@hotmail.com>
2023-01-09 14:06:19 +0900

    Display clicked numbers and operator on screen and add clear button function

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
+const currentScreen = document.getElementById('current-screen');
+const previousScreen = document.getElementById('previous-screen');
+const numberButtons = document.querySelectorAll('.number-btn');
+const operatorButtons = document.querySelectorAll(".operator-btn");
+const clearButton = document.getElementById('clear-btn');
+const equalButton = document.getElementById('equal-btn');
+
+let num1 = '';
+let num2 = '';
+let clickedOperator = '';
+let currentOperation = null;
+
+equalButton.addEventListener('click', () => {
+    num2 = currentScreen.textContent;
+    operate(clickedOperator, num1, num2);
+})
+
+function reset(){
+  num1 = '';
+  num2 = '';
+  clickedOperator = '';
+}
+
+function clearCurrentScreen(){
+  currentScreen.textContent = '';
+}
+
+function clearPreviousScreen(){
+  previousScreen.textContent = '';
+}
+
+clearButton.addEventListener('click', () =>{
+  clearCurrentScreen();
+  clearPreviousScreen();
+  reset();
+});
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => appendNumber(button.textContent))
+})
+
+function appendNumber(number) {
+    currentScreen.textContent += number;
+}
+
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        num1 = currentScreen.textContent;
+        clickedOperator = button.textContent;
+        clearCurrentScreen();
+        appendInputToPreviousScreen(num1 + button.textContent);
+    })
+})
+
+function appendInputToPreviousScreen(input){
+  previousScreen.textContent += input;
+}
+
+function appendOperator(operator){
+    currentScreen.textContent += operator;
+}
+
 function add (a, b) {
 	return a + b;
 };
